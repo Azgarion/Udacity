e6d506a0f116e79eea9cd0863da54d2a6a59d31a
Azgarion <arthur.td@hotmail.fr>
2018-09-10 13:59:45 +0200

    Hello

--- ud120-projects/validation/validate_poi.py
+++ ud120-projects/validation/validate_poi.py
@@ -12,21 +12,46 @@
 
 import pickle
 import sys
-sys.path.append("../tools/")
+import numpy
+
+from time import time
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+sys.path.append("../tools/")
 
-### first element is our labels, any added elements are predictor
-### features. Keep this the same for the mini-project, but you'll
-### have a different feature list when you do the final project.
+data_dict = pickle.load(
+    open("../final_project/final_project_dataset.pkl", "r"))
+
+# first element is our labels, any added elements are predictor
+# features. Keep this the same for the mini-project, but you'll
+# have a different feature list when you do the final project.
+
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+# Split data into train and test set
 
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels,
+    test_size=0.3, random_state=42)
 
-### it's all yours from here forward!  
+# it's all yours from here forward!   = preprocess()
 
+clf = tree.DecisionTreeClassifier()
 
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "training time:", round(time() - t0, 3), "s"
+
+acc = accuracy_score(pred, labels_test)
+
+print acc
